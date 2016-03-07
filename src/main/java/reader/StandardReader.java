46865d68a1202d7ac1fc5911331b08293ee25d7c
Junhong Wu <jhwu@thoughtworks.com>
2016-03-07 22:02:38 +0800

    parse non standard barcode

--- src/main/java/reader/StandardReader.java
+++ src/main/java/reader/StandardReader.java
@@ -1,8 +1,10 @@
 package reader;
 
+import com.google.common.base.Splitter;
 import org.json.JSONArray;
 
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 public class StandardReader implements Reader {
@@ -12,16 +14,37 @@ public class StandardReader implements Reader {
 
         JSONArray jsonArray = new JSONArray(input);
         for (int i = 0; i < jsonArray.length(); i++) {
-            String barcode = jsonArray.getString(i);
-            Integer amount = productMap.get(barcode);
-
-            if (amount == null) {
-                productMap.put(barcode, 1);
+            String barCode = jsonArray.getString(i);
+            if (barCode.contains("-")) {
+                parseNonStandardBarcode(productMap, barCode);
             } else {
-                productMap.put(barcode, amount + 1);
+                parseStandardBarcode(productMap, barCode);
             }
         }
 
         return productMap;
+    }
+
+    private void parseNonStandardBarcode(Map<String, Integer> productMap, String barCodeString) {
+        List<String> barCodeAndAmount = Splitter.on("-").splitToList(barCodeString);
+        String barCode = barCodeAndAmount.get(0);
+        Integer amount = Integer.valueOf(barCodeAndAmount.get(1));
+
+        Integer originalAmount = productMap.get(barCodeAndAmount.get(0));
+        if (originalAmount == null) {
+            productMap.put(barCode, amount);
+        } else {
+            productMap.put(barCode, amount + originalAmount);
+        }
+    }
+
+    private void parseStandardBarcode(Map<String, Integer> productMap, String barCode) {
+        Integer amount = productMap.get(barCode);
+
+        if (amount == null) {
+            productMap.put(barCode, 1);
+        } else {
+            productMap.put(barCode, amount + 1);
+        }
     }
 }
--- src/test/java/reader/StandardReaderTest.java
+++ src/test/java/reader/StandardReaderTest.java
@@ -25,4 +25,14 @@ public class StandardReaderTest {
         assertThat(productMap.get("ITEM000001"), is(5));
         assertThat(productMap.get("ITEM000005"), is(3));
     }
+
+    @Test
+    public void should_return_product_map_when_there_is_nonstandard_barcode() throws Exception {
+        String inputJson = "['ITEM000001','ITEM000001','ITEM000001','ITEM000001','ITEM000001','ITEM000003-2','ITEM000005','ITEM000005','ITEM000005']";
+        Map<String, Integer> productMap = standardReader.readFromCashRegister(inputJson);
+
+        assertThat(productMap.get("ITEM000001"), is(5));
+        assertThat(productMap.get("ITEM000003"), is(2));
+        assertThat(productMap.get("ITEM000005"), is(3));
+    }
 }
